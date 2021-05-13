09793c5c5be4d27b990d78e1c47c4031d7083759
Mauricio <mau9977@gmail.com>
2021-05-12 23:38:37 -0500

    feat: API producto implementada

--- src/main/java/com/platzi/market/persistence/entity/Producto.java
+++ src/main/java/com/platzi/market/persistence/entity/Producto.java
@@ -10,20 +10,34 @@ public class Producto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name= "id_producto")
     private Long idProducto;
+
     private String nombre;
+
     @Column(name= "id_categoria")
     private Long idCategoria;
+
     @Column(name = "codigo_barras")
     private String codigoBarras;
+
     @Column(name = "precio_venta")
     private Double precioVenta;
+
     @Column(name = "cantidad_stock")
     private Integer cantidadStock;
+
     private Boolean estado;
 
     @ManyToOne
     @JoinColumn(name = "id_categoria", insertable = false, updatable = false)
     private Categoria categoria;
+
+    public Categoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(Categoria categoria) {
+        this.categoria = categoria;
+    }
 
     public Long getIdProducto() {
         return idProducto;
